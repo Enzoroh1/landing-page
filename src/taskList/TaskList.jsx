0c13910d683bc9818c7be478fa04a9bc81a2232e
Enzoroh1 <enzoroh1@hotmail.com>
2024-04-25 21:57:02 -0300

    the end

--- src/taskList/TaskList.jsx
+++ src/taskList/TaskList.jsx
@@ -1,21 +1,35 @@
+import { useState } from "react";
+
 const Items = ({ nombre, visto }) => {
-  return(
+  return (
     <li>
-      {nombre} 
-      {visto ? "👍" : "😒"}
-      </li>
-  )
-}
+      {nombre}
+      {visto && "👍"}
+    </li>
+  );
+};
 
 export const TaskList = () => {
+
+  const addTask = () => {
+    setArreglo([...arreglo, {nombre:"nuevo", visto:false}])
+  }
+
+  let listadoSecciones = [
+    { nombre: "limpiar", visto: true},
+    { nombre: "comprar", visto: true},
+    { nombre: "trabajar", visto: true}
+
+  ]
+  const [arreglo, setArreglo] = useState(listadoSecciones)
+  
   return (
     <>
-    <h1>App List</h1>
-    <ol>
-      <Items nombre="Sergie" visto={true}></Items>
-      <Items nombre="Midu" visto={true}></Items>
-      <Items nombre="Gonzi" visto={false}></Items>
-    </ol>
+      <h1>App List</h1>
+      <ol>
+        {arreglo.map( item => <Items key={item.nombre} nombre={item.nombre} visto={item.visto}></Items>)}
+      </ol>
+      <button onClick={() => addTask()}>Agregar tarea</button>
     </>
-  )
-}
+  );
+};
